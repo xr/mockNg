6b9f651b9c48c6cd6c8fb09b3b3ef588b66dc581
liang <liang@bonusway.com>
2017-10-06 23:12:58 +0300

    implement scope ,  and dirty-checking

--- src/scope.js
+++ src/scope.js
@@ -1,9 +1,77 @@
+const _ = require('lodash');
+
+const initWatchVal = () => {};
+
 export default class Scope {
 	constructor() {
-
+		this.$$watchers = [];
+		this.$$lastDirtyWatch = null;
 	}
 
-	test() {
-		return 1;	
+	$watch(watchFn, listenerFn, valueEq) {
+		const watcher = {
+			watchFn: watchFn,
+			listenerFn: listenerFn || (() => {}),
+			last: initWatchVal,
+			valueEq: !!valueEq
+		};
+
+		this.$$watchers.push(watcher);
+		this.$$lastDirtyWatch = null;
+
+		return () => {
+			let index = this.$$watchers.indexOf(watcher);
+			if (index >= 0) {
+				this.$$watchers.splice(index, 1);
+				this.$$lastDirtyWatch = null;
+			}
+		}
 	}
+
+	$digest() {
+		let dirty, ttl = 10;
+		this.$$lastDirtyWatch = null;
+		do {
+			dirty = this.$$digestOnce();
+			if (dirty && !(ttl--)) {
+				throw 'digest max';
+			}
+		} while (dirty);
+	}
+
+	$$digestOnce() {
+		let newVal, oldVal, dirty;
+		this.$$watchers.some((watcher) => {
+			try {
+				if (watcher) {
+					newVal = watcher.watchFn(this);
+					oldVal = watcher.last;
+
+					if (!this.$$areEqual(newVal, oldVal, watcher.valueEq)) {
+						oldVal = (oldVal === initWatchVal) ? newVal : oldVal;
+						watcher.listenerFn(newVal, oldVal, this);
+						watcher.last = newVal;
+						this.$$lastDirtyWatch = watcher;
+						dirty = true;
+					} else if (watcher === this.$$lastDirtyWatch) {
+						return true;
+					}
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		});
+
+		return dirty;
+	}
+
+	$$areEqual(newVal, oldVal, valueEq) {
+		if (valueEq) {
+			return _.isEqual(newVal, oldVal);
+		} else {
+			return Object.is(newVal,oldVal);
+		}
+	}
+
+
 }
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1,8 +1,171 @@
 import Scope from "../src/scope";
 
 describe('Scope', () => {
+	let scope;
+
+	beforeEach(() => {
+		scope = new Scope();
+	});
+
 	it('can construct', () => {
-		const scope = new Scope();
-    	expect(scope.test()).toBe(1);
+		const watchFn = () => 'wat';
+		const listnerFn = jasmine.createSpy();
+		scope.$watch(watchFn, listnerFn);
+		
+		scope.$digest();
+
+		expect(listnerFn).toHaveBeenCalled();
+	});
+
+	it('calls the watch function with the scope as argument', () => {
+		const watchFn = jasmine.createSpy();
+		const listnerFn = () => '';
+
+		scope.$watch(watchFn, listnerFn);
+		
+		scope.$digest();
+
+		expect(watchFn).toHaveBeenCalledWith(scope);
+	});
+
+	it('calls the listnerFn when the watched value changes', () => {
+		scope.a = 'a';
+		scope.counter = 0;
+
+		scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => scope.counter++);
+
+		expect(scope.counter).toBe(0);
+
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+
+		scope.a = 'b';
+		expect(scope.counter).toBe(1);
+		scope.$digest();
+		expect(scope.counter).toBe(2);
+	});
+
+	it('calls the listnerFn when the watched value is undefined', () => {
+		scope.counter = 0;
+
+		scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => scope.counter++);
+
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+	});
+
+	it('calls the listnerFn when the watched value is null', () => {
+		scope.counter = 0;
+		scope.a = null;
+
+		scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => scope.counter++);
+
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+	});
+
+	it('calls the listnerFn with newVal as oldVal the first time', () => {
+		scope.a = 'hello';
+
+		const watchFn = (scope) => scope.a;
+		const listnerFn = jasmine.createSpy();
+
+		scope.$watch(watchFn, listnerFn);
+		
+		scope.$digest();
+
+		expect(listnerFn).toHaveBeenCalledWith('hello', 'hello', scope);
+	});
+
+	it('calls the watchFn without listnerFn', () => {
+
+		const watchFn = jasmine.createSpy();
+
+		scope.$watch(watchFn);
+		
+		scope.$digest();
+
+		expect(watchFn).toHaveBeenCalled();
+	});
+
+	it('triggers the chain watchers in same digest', () => {
+		scope.a = 'a';
+
+		scope.$watch((scope) => scope.addLater, (newVal, oldVal, scope) => {
+			if (newVal) {
+				scope.checkMe = scope.a.toUpperCase();
+			}
+		});
+
+		scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => {
+			if (newVal) {
+				scope.addLater = scope.a;
+			}
+		});
+
+		scope.$digest();
+		expect(scope.checkMe).toBe('A');
+
+		scope.a = 'b';
+		scope.$digest();
+		expect(scope.checkMe).toBe('B');
+	});
+
+	it('gives up after 10', () => {
+		scope.a = 1;
+
+		scope.$watch((scope) => scope.addLater, (newVal, oldVal, scope) => {
+			if (newVal) {
+				scope.a += scope.addLater;
+			}
+		});
+
+		scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => {
+			scope.addLater = newVal;
+		});
+
+		expect((function () {
+			scope.$digest();
+		})).toThrow();
+	});
+
+	it('computes based on the value if enabled', () => {
+		scope.a = [1,2,3];
+		scope.counter = 0;
+
+		scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => {
+			scope.counter++;
+		}, true);
+
+		expect(scope.counter).toBe(0);
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+
+		scope.a = [1,2,3];
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+
+		scope.a = scope.a.push(3);
+		scope.$digest();
+		expect(scope.counter).toBe(2);
+	});
+
+	it('allows destroying a $watch', () => {
+		scope.a = 'a';
+		scope.counter = 0;
+
+		let destroyWatchFn = scope.$watch((scope) => scope.a, (newVal, oldVal, scope) => {
+			scope.counter++;
+		});
+
+		expect(scope.counter).toBe(0);
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+
+		destroyWatchFn();
+		scope.$digest();
+		expect(scope.counter).toBe(1);
 	});
 });
